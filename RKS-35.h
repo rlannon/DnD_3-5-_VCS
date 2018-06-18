ea3c28dbe8d99f8db342eb9c66ce4994910cf1aa
Riley <lannon.riley@gmail.com>
2018-06-17 21:13:40 -0500

    Move to Library Format
    - Moved files into "include" folder
    - Deleted "main.cpp"
    - Added "RKS-35.h"
    - Added "doc" folder
    - Updated README.md

---
+++ RKS-35.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// classes
+#include "include/Character.h"
+#include "include/CharacterClass.h"
+#include "include/ClassData.h"
+#include "include/Race.h"
+#include "include/Skill.h"
+#include "include/Spell.h"
+#include "include/Item.h"
+#include "include/Weapon.h"
+#include "include/Armor.h"
+
+// formats
+#include "include/FundamentalDataTypes.h"
+#include "include/RVC.h"
+#include "include/RVR.h"
+#include "include/RVO.h"
+#include "include/VCS.h"
+#include "include/SkillsFormat.h"
